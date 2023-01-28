Fixes to raw handling: now correctly assigns channels, and now gets full precision

tests/io_tests.py
@@ -32,8 +32,8 @@ class IOTest(unittest.TestCase):
         '''
         path = os.path.join(os.path.dirname(__file__), 'data/raw_example.arw')
         image = v2v.load(path)
-        should_be = np.array([128, 128, 130, 133, 134, 134, 134, 134.]) / 256
-        self.assertTrue((image[:8, 0, 0] == should_be).all())
+        should_be = np.array([456, 456, 474, 492, 493, 494, 494, 494.]) / 15360
+        self.assertTrue((np.abs(image[:8, 0, 2] - should_be) < 1e-7).all())
 
     def test_load_and_save(self):
         '''
@@ -55,7 +55,7 @@ class IOTest(unittest.TestCase):
             # Although save can write grayscale fine, it will be converted to
             # BGR on read by cv2, and this cannot be avoided as per:
             #
-            # https://stackoverflow.com/questoins/11159506/opencv-videocapture-set-greyscale
+            # https://stackoverflow.com/questions/11159506/opencv-videocapture-set-greyscale
             #
             # Not yet sure if it's SAVED as grayscale but just cast to BGR on
             # load, or if it's converted to BGR on save.

video2vision/io.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from glob import glob
 import os
+from statistics import mean
 from typing import Dict, Iterator, List, Optional, Tuple, Union
 
 import cv2
@@ -33,7 +34,9 @@ _VIDEO_FPS = 23.976023976023978
 # to apply a Pipeline to images in memory instead of running the normal way.
 _READ_WRITE_FROM_TO_BUFFER = False
 
-_IMAGE_EXTENSIONS = ['arw', 'jpeg', 'jpg', 'mp4', 'png', 'raw', 'tif', 'tiff']
+_IMAGE_EXTENSIONS = [
+    'arw', 'jpeg', 'jpg', 'mp4', 'nef', 'png', 'raw', 'tif', 'tiff'
+]
 
 
 def load(path: str) -> np.ndarray:
@@ -53,6 +56,8 @@ def load(path: str) -> np.ndarray:
         if not has_tiff:
             raise ImportError('tifffile is needed to read tif files')
         image = tifffile.imread(path)
+        # Rescale to [0, 1] and float32
+        image = image.astype(np.float32) / 256.
 
     elif path.lower().endswith('.mp4'):
         reader = cv2.VideoCapture(path)
@@ -63,18 +68,38 @@ def load(path: str) -> np.ndarray:
                 frames.append(frame)
         reader.release()
         image = np.stack(frames, axis=2)
+        # Rescale to [0, 1] and float32
+        image = image.astype(np.float32) / 256.
 
-    elif path.lower().endswith('.arw'):
+    elif path.lower().endswith(('.arw', '.nef')):
         if not has_rawpy:
             raise ImportError('rawpy is needed to read arw files')
+
         with rawpy.imread(path) as raw_file:
-            image = raw_file.postprocess()
+            image = raw_file.postprocess(
+                # Prevents gamma correction
+                gamma=(1, 1),
+                output_color=rawpy.ColorSpace.raw,
+                no_auto_scale=True,
+                no_auto_bright=True,
+                # Output will be maximum precision
+                output_bps=16,
+            )
+            white_level = np.array([
+                raw_file.camera_white_level_per_channel[0],
+                mean((raw_file.camera_white_level_per_channel[1],
+                      raw_file.camera_white_level_per_channel[3])),
+                raw_file.camera_white_level_per_channel[2],
+            ])
+        # Rescales to [0, 1] and float32
+        image = image.astype(np.float32) / white_level.reshape(1, 1, 3)
+        # Reverse channels from RGB to BGR
+        image = image[:, :, ::-1]
 
     else:
         image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-
-    # Rescale to [0, 1] and float32
-    image = image.astype(np.float32) / 256.
+        # Rescale to [0, 1] and float32
+        image = image.astype(np.float32) / 256.
 
     return _coerce_to_image(image)
 
@@ -115,6 +140,9 @@ def save(image: np.ndarray, path: str):
             raise ImportError('tifffile is needed to write tif files')
         # photometric='minisblack' suppresses a DeprecationWarning.
         tifffile.imwrite(path, image, photometric='minisblack')
+
+    elif path.lower().endswith(('.nef', '.arw')):
+        raise NotImplementedError('Saving in raw form is not supported')
 
     else:
         return cv2.imwrite(path, image)